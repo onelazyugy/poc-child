dde05e248f7b6f442a62e7c4b322c8f1484bdb4e
onelazyugy <you@example.com>
2018-09-11 08:50:20 -0400

    done child poc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'child app';
   message = '';
+  messageFromParent = '';
+
+  ngOnInit(): void {
+    if (window.addEventListener) {
+      window.addEventListener('message', (event) => {
+        const dataFromChildIframe = event.data;
+        console.log(dataFromChildIframe);
+        this.messageFromParent = dataFromChildIframe.messageFromParent;
+      });
+    }
+  }
 
   submit = () => {
     console.log(this.message);
     parent.postMessage(
     {
-      messageFromParent: this.message
+      messageFromChild: this.message
     },
     'http://localhost:4201/');
   }
