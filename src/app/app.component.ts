b2efa1775e635d1aafbac07e363a3153738592b8
onelazyugy <you@example.com>
2018-09-11 14:24:19 -0400

    testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,8 +23,8 @@ export class AppComponent implements OnInit{
   submit = () => {
     console.log(this.message);
     const localhostParentURL = 'http://localhost:4201/';
-    // const parentURL = 'http://mfp.primericaonline.com:9080/priapp/index.html#/';
-    const parentURL = 'https://dev.primericaonline.com';
+    const parentURL = 'http://mfp.primericaonline.com:9080/priapp/index.html#/';
+    // const parentURL = 'https://dev.primericaonline.com';
     parent.postMessage(
     {
       messageFromChild: this.message
